768c8ab2cab376172d6d38815bd9c729890446bc
elddiablo <elddiablo001@gmail.com>
2019-02-09 13:36:56 -0600

    + refractoring continued

--- src/components/store/reducers/order.js
+++ src/components/store/reducers/order.js
@@ -44,22 +44,14 @@ const fetchOrdersFail = (state) => {
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.PURCHASE_INIT:
-            return purchaseInit(state, action);
-        case actionTypes.PURCHASE_BURGER_SUCCESS:
-            return purchaseBurgerSuccess(state, action);
-        case actionTypes.PURCHASE_BURGER_FAIL:
-            return purchaseBurgerFail(state);
-        case actionTypes.PURCHASE_BURGER_START: 
-            return purchaseBurgerStart(state);
-        case actionTypes.FETCH_ORDERS_START:
-            return fetchOrdersStart(state);
-        case actionTypes.FETCH_ORDERS_SUCCESS:
-            return fetchOrdersSuccess(state, action);
-        case actionTypes.FETCH_ORDERS_FAIL:
-            return fetchOrdersFail(state);
-        default:
-            return state;
+        case actionTypes.PURCHASE_INIT: return purchaseInit(state, action);
+        case actionTypes.PURCHASE_BURGER_SUCCESS: return purchaseBurgerSuccess(state, action);
+        case actionTypes.PURCHASE_BURGER_FAIL: return purchaseBurgerFail(state);
+        case actionTypes.PURCHASE_BURGER_START: return purchaseBurgerStart(state);
+        case actionTypes.FETCH_ORDERS_START: return fetchOrdersStart(state);
+        case actionTypes.FETCH_ORDERS_SUCCESS: return fetchOrdersSuccess(state, action);
+        case actionTypes.FETCH_ORDERS_FAIL: return fetchOrdersFail(state);
+        default: return state;
     }
 }
 
