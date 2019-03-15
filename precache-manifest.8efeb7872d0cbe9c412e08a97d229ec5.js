1efa4a07fca22680c9c0977d54df076021f840be
elddiablo <elddiablo001@gmail.com>
2019-03-15 11:13:54 -0500

    Updates

---
+++ precache-manifest.8efeb7872d0cbe9c412e08a97d229ec5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Burger-Builder-React.js/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "465b6de265d041f23803",
+    "url": "/Burger-Builder-React.js/static/js/runtime~main.465b6de2.js"
+  },
+  {
+    "revision": "b38636a70b8afad59ac5",
+    "url": "/Burger-Builder-React.js/static/js/main.b38636a7.chunk.js"
+  },
+  {
+    "revision": "24052662a45fdd401226",
+    "url": "/Burger-Builder-React.js/static/js/1.24052662.chunk.js"
+  },
+  {
+    "revision": "b38636a70b8afad59ac5",
+    "url": "/Burger-Builder-React.js/static/css/main.12d1dec1.chunk.css"
+  },
+  {
+    "revision": "0c0580a700965f401ea9c1b14c35a061",
+    "url": "/Burger-Builder-React.js/index.html"
+  }
+];
